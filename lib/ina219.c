f066ce79f1f894eb45d365d0ca22e5a81674edd9
zorxx <zorxx@zorxx.com>
2025-12-29 15:33:33 -0600

    Fixes

--- lib/ina219.c
+++ lib/ina219.c
@@ -35,11 +35,11 @@
 #define INA219_CFG_BIT_BADC0 7
 #define INA219_CFG_BIT_SADC0 3
 #define INA219_CFG_BIT_MODE  0
-#define INA219_CFG_MASK_PG   (3   << INA219_CFG_BIT_PG0)
-#define INA219_CFG_MASK_BADC (0xf << INA219_CFG_BIT_BADC0)
-#define INA219_CFG_MASK_SADC (0xf << INA219_CFG_BIT_SADC0)
-#define INA219_CFG_MASK_MODE (7   << INA219_CFG_BIT_MODE)
-#define INA219_CFG_MASK_BRNG (1   << INA219_CFG_BIT_BRNG)
+#define INA219_CFG_MASK_PG   0x3
+#define INA219_CFG_MASK_BADC 0xf
+#define INA219_CFG_MASK_SADC 0xf
+#define INA219_CFG_MASK_MODE 0x7
+#define INA219_CFG_MASK_BRNG 0x1
 
 static const float u_shunt_max[] =
 {
@@ -59,8 +59,8 @@ typedef struct
    ina219_resolution_t shunt_res;
    ina219_mode_t mode;
    float r_shunt;
-   float i_lsb;
-   float p_lsb;
+   double i_lsb;
+   double p_lsb;
    bool calibrated;
 } ina219_context_t;
 
@@ -69,13 +69,13 @@ static __inline bool ina219_read(ina219_context_t *ctx, uint8_t reg, uint16_t *r
    uint8_t data[2];
    if(!i2c_ll_read_reg(ctx->i2c_ctx, reg, data, sizeof(data)))
       return false;
-   *result = (uint16_t) (data[0] << 8) | (data[1] >> 8);
+   *result = (((uint16_t)data[0]) << 8) | data[1];
    return true;
 }
 
 static __inline bool ina219_write(ina219_context_t *ctx, uint8_t reg, uint16_t value)
 {
-   uint8_t data[2] = { value >> 8, value };
+   uint8_t data[2] = { (value >> 8) & 0xFF, value & 0xFF };
    return i2c_ll_write_reg(ctx->i2c_ctx, reg, data, sizeof(data));
 }
 
@@ -90,11 +90,11 @@ static __inline bool ina219_get_config_bits(ina219_context_t *ctx, uint16_t mask
 }
 
 #define ina219_get_config(ctx) \
- ((((uint16_t)((ctx)->bus_range)) << INA219_CFG_BIT_BRNG)  |  \
-  (((uint16_t)((ctx)->gain))      << INA219_CFG_BIT_PG0)   |  \
-  (((uint16_t)((ctx)->bus_res))   << INA219_CFG_BIT_BADC0) |  \
-  (((uint16_t)((ctx)->shunt_res)) << INA219_CFG_BIT_SADC0) |  \
-  (((uint16_t)((ctx)->mode))      << INA219_CFG_BIT_MODE))
+ ((((uint16_t)((ctx)->bus_range & INA219_CFG_MASK_BRNG)) << INA219_CFG_BIT_BRNG)  |  \
+  (((uint16_t)((ctx)->gain      & INA219_CFG_MASK_PG))   << INA219_CFG_BIT_PG0)   |  \
+  (((uint16_t)((ctx)->bus_res   & INA219_CFG_MASK_BADC)) << INA219_CFG_BIT_BADC0) |  \
+  (((uint16_t)((ctx)->shunt_res & INA219_CFG_MASK_SADC)) << INA219_CFG_BIT_SADC0) |  \
+  (((uint16_t)((ctx)->mode      & INA219_CFG_MASK_MODE)) << INA219_CFG_BIT_MODE))
 
 /* --------------------------------------------------------------------------------------------------------
  * Exported Functions
@@ -134,17 +134,30 @@ ina219_t ina219_init(i2c_lowlevel_config *config, uint8_t i2c_address,
    }
    else
    {
+      uint16_t cfg, verify;
       ctx->bus_range = bus_range;
       ctx->gain = gain;
       ctx->bus_res = bus_res;
       ctx->shunt_res = shunt_res;
       ctx->mode = mode;
-      if(!ina219_write(ctx, INA219_REG_CONFIG, ina219_get_config(ctx)))
+      cfg = ina219_get_config(ctx);
+      if(!ina219_write(ctx, INA219_REG_CONFIG, cfg | (((uint16_t)1) << INA219_CFG_BIT_RST))
+      || !ina219_write(ctx, INA219_REG_CONFIG, cfg))
       {
          SERR("[%s] Failed to write device configuration", __func__);
       }
+      else if(!ina219_read(ctx, INA219_REG_CONFIG, &verify))
+      {
+         SERR("[%s] Failed to read-back configuration", __func__);
+      }
+      else if(cfg != verify)
+      {
+         SERR("[%s] Failed to verify configuration (received 0x%04x, expected 0x%04x)",
+            __func__, verify, cfg);
+      }
       else
       {
+         SDBG("[%s] Wrote config 0x%04x", __func__, cfg);
          return ctx;  // success
       }
    }
@@ -167,13 +180,9 @@ bool ina219_calibrate(ina219_t ina219, float r_shunt)
    ina219_context_t *ctx = (ina219_context_t *) ina219;
    uint16_t cal;
 
-   ctx->i_lsb = (uint16_t)(u_shunt_max[ctx->gain] / r_shunt / 32767 * 100000000);
-   ctx->i_lsb /= 100000000;
-   ctx->i_lsb /= 0.0001;
-   ctx->i_lsb = ceil(ctx->i_lsb);
-   ctx->i_lsb *= 0.0001;
+   ctx->i_lsb = (double)(u_shunt_max[ctx->gain]) / 32768.0;
    ctx->p_lsb = ctx->i_lsb * 20;
-   cal = (uint16_t)((0.04096) / (ctx->i_lsb * r_shunt));
+   cal = (uint16_t)trunc(0.04096 / (ctx->i_lsb * r_shunt));
 
    if(!ina219_write(ctx, INA219_REG_CALIBRATION, cal))
    {
@@ -211,39 +220,42 @@ bool ina219_get_bus_voltage(ina219_t ina219, float *voltage)
    }
 
    *voltage = (value >> 3) * 0.004;
+   SDBG("[%s] %.3f V (0x%04x)", __func__, *voltage, value);
    return true;
 }
 
 bool ina219_get_shunt_voltage(ina219_t ina219, float *voltage)
 {
    ina219_context_t *ctx = (ina219_context_t *) ina219;
-   uint16_t value;
+   int16_t value; /* 2's complient, allowing negative voltage readings */
 
-   if(!ina219_read(ctx, INA219_REG_SHUNT_VOLTAGE, &value))
+   if(!ina219_read(ctx, INA219_REG_SHUNT_VOLTAGE, (uint16_t*)&value))
    {
       SERR("[%s] Failed to read", __func__);
       return false;
    }
 
-   *voltage = value / 100000.0;
+   *voltage = (double)value / 100000.0;
+   SDBG("[%s] %.3f mV (0x%04x)", __func__, (*voltage) * 1000.0, value);
    return true;
 }
 
 bool ina219_get_current(ina219_t ina219, float *current)
 {
    ina219_context_t *ctx = (ina219_context_t *) ina219;
-   uint16_t value;
+   int16_t value; /* 2's complient, allowing negative current readings */
 
    if(!ctx->calibrated)
       return false;
 
-   if(!ina219_read(ctx, INA219_REG_CURRENT, &value))
+   if(!ina219_read(ctx, INA219_REG_CURRENT, (uint16_t*)&value))
    {
       SERR("[%s] Failed to read", __func__);
       return false;
    }
 
    *current = value * ctx->i_lsb;
+   SDBG("[%s] %.3f mA (0x%04x)", __func__, (*current) * 1000.0, value);
    return true;
 }
 
@@ -262,5 +274,6 @@ bool ina219_get_power(ina219_t ina219, float *power)
    }
 
    *power = value * ctx->p_lsb;
+   SDBG("[%s] %.3f mW (0x%04x)", __func__, (*power) * 1000.0, value);
    return true;
 }
